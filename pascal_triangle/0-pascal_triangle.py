cff1cb6f71f8e71a46617f40c94edc7382ec6f02
Chihaan <gavet.vadim@gmail.com>
2026-05-11 12:29:15 +0200

    add: pascal_triangle.py

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -2,3 +2,14 @@
 
 
 def pascal_triangle(n):
+    triangle = []
+    if n <= 0:
+        return  triangle
+    triangle = [[1]]
+    for i in range(1, n):
+        prev_line = triangle[-1]
+        triangle.append([1])
+        for j in range(len(prev_line) - 1):
+            triangle[-1].append(prev_line[j] + prev_line[j + 1])
+        triangle[-1].append(1)
+    return triangle
