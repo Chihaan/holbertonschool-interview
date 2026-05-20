d0d93523f483b7c2a4e5b60ff17e750320279fca
Chihaan <gavet.vadim@gmail.com>
2026-05-20 10:50:46 +0200

    add: number of boxes for unexisting keys

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -2,8 +2,10 @@
 """Function to unlock boxes"""
 
 
-def canUnlockAll(boites):
+def canUnlockAll(boxes):
     """Start of the function"""
+
+    n = len(boxes)
     # Une liste de boites a explorer en partant de 0 ouverte
     a_explorer = [0]
     # Une liste des boites où je suis allé
@@ -14,14 +16,14 @@ def canUnlockAll(boites):
         # Je pop la boite
         boite = a_explorer.pop()
         # Je vais a la boite de ma clef
-        for clef in boites[boite]:
+        for clef in boxes[boite]:
             # Si cette boite n'est pas dans mes boites ouvertes
-            if clef not in boites_ouvertes:
+            if clef not in boites_ouvertes and clef < n:
                 # Je l'ajoute a ma liste de boite a ouvrir
                 a_explorer.append(clef)
                 # J'ajoute la clef a mes boites ouvertes
                 boites_ouvertes.add(clef)
     # Si la taille de la liste boites est égale a celle de mes boites ouvertes
-    return len(boites) == len(boites_ouvertes)
+    return n == len(boites_ouvertes)
     # True si j'ai ouvert toutes les boites
     # False, il manquait une clef
