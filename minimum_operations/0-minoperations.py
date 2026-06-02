14c99832fa4609acfc8f3d93bf0c587bd5b2a0fb
Chihaan <gavet.vadim@gmail.com>
2026-06-02 09:27:05 +0200

    Fixed comments minoperations

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python3
+"""Calcule le nombre minimum d'opérations (Copy All + Paste) pour
+obtenir exactement n caractères à partir d'un seul caractère.
+"""
 
 
 def minOperations(n):
-    """Retourne le nombre minimum d'opérations pour obtenir n caractères."""
+    """Retourne le nombre minimum d'opérations pour obtenir n caractères.
+
+    Le résultat est la somme des facteurs premiers de n.
+    Retourne 0 si n est impossible à atteindre (n <= 1).
+    """
     diviseur = 2
     operations = 0
 
-    if n == 0 or n == 1 :
+    if n == 0 or n == 1:
         return 0
 
     while n > 1:
@@ -15,4 +22,5 @@ def minOperations(n):
             operations += diviseur
         else:
             diviseur += 1
+
     return operations
