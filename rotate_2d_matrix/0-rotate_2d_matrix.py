a8702203ae312e1bc19deaa7711b5f6118b99ada
Chihaan <gavet.vadim@gmail.com>
2026-07-01 10:34:46 +0200

    add: 2D Matrix algo

--- rotate_2d_matrix/0-rotate_2d_matrix.py
+++ rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Rotate a 2D matrix"""
 
+
 def rotate_2d_matrix(matrix):
     """
     Rotate a 2D matrix 90 degrees clockwise
@@ -11,7 +12,7 @@ def rotate_2d_matrix(matrix):
     n = len(matrix)
 
     for i in range(n):
-        for j in range(i , n):
+        for j in range(i, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
     for i in range(n):
