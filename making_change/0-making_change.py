2f96f014f64ad3cd631d7a6c02223ce85f9dde48
Chihaan <gavet.vadim@gmail.com>
2026-07-07 13:12:00 +0200

    add: file interview

---
+++ making_change/0-making_change.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Making change"""
+
+
+def makeChange(coins, total):
+    """determine the fewest number of coins needed"""
+    if total <= 0:
+        return 0
+
+    dp = [0] + [float('inf')] * total
+
+    for amount in range(1, total + 1):
+        for coin in coins:
+            if coin <= amount and dp[amount - coin] + 1 < dp[amount]:
+                dp[amount] = dp[amount - coin] + 1
+
+    return dp[total] if dp[total] != float('inf') else - 1
+
